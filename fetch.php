f0ee7fc78e30c7a3309a4fe70509b3d9fffb9ce5
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-24 20:57:14 -0400

    Create fetch.php

---
+++ fetch.php
@@ -0,0 +1,72 @@
+<?php
+//fetch.php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'assignment5');
+define('DB_PASSWORD', 'kostin');
+define('DB_NAME', 'assignment5');
+ 
+/* Attempt to connect to MySQL database */
+$connect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$columns = array('first_name', 'last_name');
+
+$query = "SELECT * FROM user ";
+
+if(isset($_POST["search"]["value"]))
+{
+ $query .= '
+ WHERE first_name LIKE "%'.$_POST["search"]["value"].'%" 
+ OR last_name LIKE "%'.$_POST["search"]["value"].'%" 
+ ';
+}
+
+if(isset($_POST["order"]))
+{
+ $query .= 'ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
+ ';
+}
+else
+{
+ $query .= 'ORDER BY id DESC ';
+}
+
+$query1 = '';
+
+if($_POST["length"] != -1)
+{
+ $query1 = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+}
+
+$number_filter_row = mysqli_num_rows(mysqli_query($connect, $query));
+
+$result = mysqli_query($connect, $query . $query1);
+
+$data = array();
+
+while($row = mysqli_fetch_array($result))
+{
+ $sub_array = array();
+ $sub_array[] = '<div contenteditable class="update" data-id="'.$row["id"].'" data-column="first_name">' . $row["first_name"] . '</div>';
+ $sub_array[] = '<div contenteditable class="update" data-id="'.$row["id"].'" data-column="last_name">' . $row["last_name"] . '</div>';
+ $sub_array[] = '<button type="button" name="delete" class="btn btn-danger btn-xs delete" id="'.$row["id"].'">Delete</button>';
+ $data[] = $sub_array;
+}
+
+function get_all_data($connect)
+{
+ $query = "SELECT * FROM user";
+ $result = mysqli_query($connect, $query);
+ return mysqli_num_rows($result);
+}
+
+$output = array(
+ "draw"    => intval($_POST["draw"]),
+ "recordsTotal"  =>  get_all_data($connect),
+ "recordsFiltered" => $number_filter_row,
+ "data"    => $data
+);
+
+echo json_encode($output);
+
+?>
