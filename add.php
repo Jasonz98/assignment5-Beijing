ccc6d8640564ba7fb25ad119ed88d07006b41251
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-29 18:07:03 -0400

    Create add.php

---
+++ add.php
@@ -0,0 +1,162 @@
+<html>
+<head>
+    <title>Add Data</title>
+</head>
+ 
+<body>
+<?php
+//including the database connection file
+include_once("config.php");
+ 
+if(isset($_POST['Submit'])) {    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $team = $_POST['team'];
+        
+    // checking empty fields
+    if(empty($name) || empty($email) || empty($team)) {                
+        if(empty($name)) {
+            echo "<font color='red'>Name field is empty.</font><br/>";
+        }
+        
+        if(empty($email)) {
+            echo "<font color='red'>Email field is empty.</font><br/>";
+        }
+        
+        if(empty($team)) {
+            echo "<font color='red'>Team field is empty.</font><br/>";
+        }
+        
+        //link to the previous page
+        echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+    } else { 
+        // if all the fields are filled (not empty)             
+        //insert data to database
+        $result = mysqli_query($mysqli, "INSERT INTO users(name,age,email) VALUES('$name','$email','$team')");
+        
+        //display success message
+        echo "<font color='green'>Data added successfully.";
+        echo "<br/><a href='index.php'>View Result</a>";
+    }
+}
+?>
+</body>
+</html>
+Data from database is fetched and displayed in index.php file. This is our homepage. This file also contains a link to add data. On every row of displayed data, there is also a link to edit and delete data. Below is a sample image of our homepage:
+
+CRUD PHP MySQL
+
+index.php
+
+
+<?php
+//including the database connection file
+include_once("config.php");
+
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+?>
+
+<html>
+<head>	
+    <title>Homepage</title>
+</head>
+
+<body>
+    <a href="add.html">Add New Data</a><br/><br/>
+
+    <table width='80%' border=0>
+        <tr bgcolor='#CCCCCC'>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Team</td>
+            <td>Update</td>
+        </tr>
+        <?php 
+        //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+        while($res = mysqli_fetch_array($result)) { 		
+            echo "<tr>";
+            echo "<td>".$res['name']."</td>";
+            echo "<td>".$res['email']."</td>";
+            echo "<td>".$res['team']."</td>";	
+            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+        }
+        ?>
+    </table>
+</body>
+</html>
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+<?php
+//including the database connection file
+include_once("config.php");
+ 
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+?>
+ 
+<html>
+<head>    
+    <title>Homepage</title>
+</head>
+ 
+<body>
+    <a href="add.html">Add New Data</a><br/><br/>
+ 
+    <table width='80%' border=0>
+        <tr bgcolor='#CCCCCC'>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Team</td>
+            <td>Update</td>
+        </tr>
+        <?php 
+        //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+        while($res = mysqli_fetch_array($result)) {         
+            echo "<tr>";
+            echo "<td>".$res['name']."</td>";
+            echo "<td>".$res['email']."</td>";
+            echo "<td>".$res['team']."</td>";    
+            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
+        }
+        ?>
+    </table>
+</body>
+</html>
