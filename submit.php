d2ceebeb0041674dcfa07a53e73afae5b0440421
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-29 18:50:21 -0400

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,9 @@
+<?php  
+	$connect = mysqli_connect("localhost", "urcscon3_", "coffee1N", "urcscon3_phoeni");
+	$vote = $_POST['vote'];
+	
+	$sql = "INSERT INTO vote(vote) VALUES ('$vote')";
+	mysqli_query($connect, $sql);
+	header("location: thankyou.php");
+	mysqli_close($connect); 
+ ?>
