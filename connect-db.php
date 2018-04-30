500605e84f4a81c8fa600793678ab427135ab0ae
mresnikoff <35782180+mresnikoff@users.noreply.github.com>
2018-04-30 12:29:06 -0400

    Update connect-db.php

--- connect-db.php
+++ connect-db.php
@@ -1,16 +1,28 @@
 <?php
-// Database Variables (edit with your own server information)
-$server = 'localhost';
-$user = 'urcscon3_olicode';
-$pass = 'benoudrih';
-$db = 'urcscon3_beijing5';
-
-// Connect to Database
-$connection = mysqli_connect($server,$user,$pass,$db);
-if (!$connection) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+/*
+// mysql_connect("database-host", "username", "password")
+$conn = mysql_connect("66.147.242.186","urcscon3_beijing5","urcscon3_olicode") 
+            or die("cannot connected");
+ 
+// mysql_select_db("database-name", "connection-link-identifier")
+@mysql_select_db("test",$conn);
+*/
+ 
+/**
+ * mysql_connect is deprecated
+ * using mysqli_connect instead
+ */
+ define('DB_SERVER', '66.147.242.186');
+define('DB_USERNAME', 'urcscon3_olicode');
+define('DB_PASSWORD', 'benoudrih');
+define('DB_NAME', 'urcscon3_beijing5');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-?>
+
+
