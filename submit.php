a8911a9f99b84f507a196f3fd1ba95e71cee1f48
mresnikoff <35782180+mresnikoff@users.noreply.github.com>
2018-04-30 12:47:08 -0400

    Update submit.php

--- submit.php
+++ submit.php
@@ -9,11 +9,47 @@
 	
 	$sql = "INSERT INTO vote(vote) VALUES ('$vote')";
 	mysqli_query($connect, $sql);
-	header("location: thanks.php");
+	
 	mysqli_close($connect); 
  ?>
 
 if($mysqli === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/menu-highlighter.js"></script>
+	<title>Survey</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/override.css">
+  <link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet">
+</head>
+<header>
+      <h1>Basketball</h1>
+      <div>
+          <a href="login.php">Sign in by clicking this!</a>
+          <div>Don't have an account?<a href="register.php"> Sign up by clicking this!</a>
+      </div>
+    </header>
+<body>
+  
+    <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+    <li><a href="history.php">History</a></li>
+    <li><a href="rules.php">Rules</a></li>
+    <li><a href="nba.php">NBA</a></li>
+    <li><a href="players.php">Players</a></li>
+    <li><a href="survey.php">Take a survey?</a></li>
+        </ul>
+      </nav> 
+  
+  <h2>Thank you for voting!</h2>
+	
+<footer>
+  Website created by Team Beijing
+</footer>
 
