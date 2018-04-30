e5af19722c9ff173cd58065eb100ce9503a5655f
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-30 13:27:32 -0400

    Update view.php

--- view.php
+++ view.php
@@ -5,44 +5,147 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     	<link rel="stylesheet" href="css/override.css">
     	<link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet">
-	<title>View Records</title>
-</head>
-<body>
-<?php
-// connect to the database
-include('connect-db.php');
-
-// get results from database
-$result = mysqli_query($connection, "SELECT * FROM accounts");
-?>
-
-<table border>
-  <tr>
-    <th>ID</th>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th></th>
-    <th></th>
-  </tr>
-<?php
-// loop through results of database query, displaying them in the table
-while($row = mysqli_fetch_array( $result )) {
-?>
-  <tr>
-    <td><?php echo $row['id']; ?></td>
-    <td><?php echo $row['firstname']; ?></td>
-    <td><?php echo $row['lastname']; ?></td>
-    <td><a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
-    <td><a href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
-  </tr>
-<?php
-// close the loop
-}
-?>
-</table>
-
-<div>
-	<a href="new.php">Add a new record</a>
-</div>
-</body>
+	<title>View Records</title>	
+  <style>
+  body
+  {
+   margin:0;
+   padding:0;
+   background-color:#f1f1f1;
+  }
+  .box
+  {
+   width:1270px;
+   padding:20px;
+   background-color:#fff;
+   border:1px solid #ccc;
+   border-radius:5px;
+   margin-top:25px;
+   box-sizing:border-box;
+  }
+  </style>
+ </head>
+ <body>
+  <div class="container box">
+   <h1 align="center">Add your information here!</h1>
+   <br />
+   <div class="table-responsive">
+   <br />
+    <div align="right">
+     <button type="button" name="add" id="add" class="btn btn-info">Add</button>
+    </div>
+    <br />
+    <div id="alert_message"></div>
+    <table id="user_data" class="table table-bordered table-striped">
+     <thead>
+      <tr>
+       <th>Frist Name</th>
+       <th>Last Name</th>
+       <th></th>
+      </tr>
+     </thead>
+    </table>
+   </div>
+  </div>
+ </body>
 </html>
+
+<script type="text/javascript" language="javascript" >
+ $(document).ready(function(){
+  
+  fetch_data();
+
+  function fetch_data()
+  {
+   var dataTable = $('#user_data').DataTable({
+    "processing" : true,
+    "serverSide" : true,
+    "order" : [],
+    "ajax" : {
+     url:"fetch.php",
+     type:"POST"
+    }
+   });
+  }
+  
+  function update_data(id, column_name, value)
+  {
+   $.ajax({
+    url:"update.php",
+    method:"POST",
+    data:{id:id, column_name:column_name, value:value},
+    success:function(data)
+    {
+     $('#alert_message').html('<div class="alert alert-success">'+data+'</div>');
+     $('#user_data').DataTable().destroy();
+     fetch_data();
+    }
+   });
+   setInterval(function(){
+    $('#alert_message').html('');
+   }, 5000);
+  }
+
+  $(document).on('blur', '.update', function(){
+   var id = $(this).data("id");
+   var column_name = $(this).data("column");
+   var value = $(this).text();
+   update_data(id, column_name, value);
+  });
+  
+  $('#add').click(function(){
+   var html = '<tr>';
+   html += '<td contenteditable id="data1"></td>';
+   html += '<td contenteditable id="data2"></td>';
+   html += '<td><button type="button" name="insert" id="insert" class="btn btn-success btn-xs">Insert</button></td>';
+   html += '</tr>';
+   $('#user_data tbody').prepend(html);
+  });
+  
+  $(document).on('click', '#insert', function(){
+   var first_name = $('#data1').text();
+   var last_name = $('#data2').text();
+   if(first_name != '' && last_name != '')
+   {
+    $.ajax({
+     url:"insert.php",
+     method:"POST",
+     data:{first_name:first_name, last_name:last_name},
+     success:function(data)
+     {
+      $('#alert_message').html('<div class="alert alert-success">'+data+'</div>');
+      $('#user_data').DataTable().destroy();
+      fetch_data();
+     }
+    });
+    setInterval(function(){
+     $('#alert_message').html('');
+    }, 5000);
+   }
+   else
+   {
+    alert("Both Fields is required");
+   }
+  });
+  
+  $(document).on('click', '.delete', function(){
+   var id = $(this).attr("id");
+   if(confirm("Are you sure you want to remove this?"))
+   {
+    $.ajax({
+     url:"delete.php",
+     method:"POST",
+     data:{id:id},
+     success:function(data){
+      $('#alert_message').html('<div class="alert alert-success">'+data+'</div>');
+      $('#user_data').DataTable().destroy();
+      fetch_data();
+     }
+    });
+    setInterval(function(){
+     $('#alert_message').html('');
+    }, 5000);
+   }
+  });
+ });
+</script>
