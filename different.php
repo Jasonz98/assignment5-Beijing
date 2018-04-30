e74a880a4db50a2156b895c18822856a750cb269
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-29 23:01:39 -0400

    Create different.php

---
+++ different.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/menu-highlighter.js"></script>
+	<title>Basketball</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/override.css">
+  <link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet">
+
+</head>
+
+<body>
+  
+  <header>
+      <h1>Basketball</h1>
+      <div>
+          <a href="logout.php">Sign out by clicking this!</a>
+          <div>Don't have an account?<a href="register.php"> Sign up by clicking this!</a>
+      </div>
+    </header>
+<nav>
+  
+  <ul>
+    
+    <li><a href="index.html">Home</a></li>
+    <li><a href="history.html">History</a></li>
+    <li><a href="rules.html">Rules</a></li>
+    <li><a href="nba.html">NBA</a></li>
+    <li><a href="players.html">Players</a></li>
+    <li><a href="survey.php">Take a survey?</a></li>
+  </ul>
+</nav>
+   
+    
+
+    <div>
+      <h2>Explore our website</h2>
+      <p>Welcome to our website about basketball. Here you can learn about the history, rules of basketball. You can see the development of NBA and you can get to know some amazing players. Also, if you are interested in Basketball, don't miss the the chance to take the survey and sign up to see what other people say. </p>
+    </div>
+
+     
+
+
+
+
+
+
+<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/menu-highlighter.js"></script>
+
+</body>
+</html>
