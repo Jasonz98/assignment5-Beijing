cc01e95007d885eafd369e790d23a21849350f33
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-30 16:30:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       <h1>Basketball</h1>
       <div>
           <a href="login.php">Sign in by clicking this!</a>
-          <div>Don't have an account?<a href="register.php"> Sign up by clicking this!</a>
+	      <div>Don't have an account?<a href="register.php"> Sign up by clicking this!</a></div>
       </div>
     </header>
 <nav>
