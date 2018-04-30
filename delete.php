3bfe0c748e4d1e13564b2130311394c5ecb923ba
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-30 13:30:55 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,21 +1,11 @@
 <?php
-// connect to the database
-include('connect-db.php');
-
-// check if the 'id' variable is set in URL, and check that it is valid
-if (isset($_GET['id']) && is_numeric($_GET['id']))
+$connect = mysqli_connect("localhost", "root", "", "testing");
+if(isset($_POST["id"]))
 {
-	// get id value
-	$id = $_GET['id'];
-
-	// delete the entry
-	$result = mysqli_query($connection, "DELETE FROM accounts WHERE id=$id");
-
-	// redirect back to the view page
-	header("Location: view.php");
-
-} else {
-	// if id isn't set, or isn't valid, redirect back to view page
-	header("Location: view.php");
+ $query = "DELETE FROM user WHERE id = '".$_POST["id"]."'";
+ if(mysqli_query($connect, $query))
+ {
+  echo 'Data Deleted';
+ }
 }
 ?>
