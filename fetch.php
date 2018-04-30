cfef86c4ef186762a71cddc0947d58134a3057a7
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-30 13:32:21 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,6 +1,12 @@
 <?php
 //fetch.php
-$connect = mysqli_connect("localhost", "root", "", "testing");
+define('DB_SERVER', '66.147.242.186');
+define('DB_USERNAME', 'urcscon3_olicode');
+define('DB_PASSWORD', 'benoudrih');
+define('DB_NAME', 'urcscon3_beijing5');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 $columns = array('first_name', 'last_name');
 
 $query = "SELECT * FROM user ";
