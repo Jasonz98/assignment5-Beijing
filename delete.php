97e31da3b8e48047aa37bd8c804dc02d3332088a
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-04-30 13:31:27 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,11 @@
 <?php
-$connect = mysqli_connect("localhost", "root", "", "testing");
+define('DB_SERVER', '66.147.242.186');
+define('DB_USERNAME', 'urcscon3_olicode');
+define('DB_PASSWORD', 'benoudrih');
+define('DB_NAME', 'urcscon3_beijing5');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if(isset($_POST["id"]))
 {
  $query = "DELETE FROM user WHERE id = '".$_POST["id"]."'";
